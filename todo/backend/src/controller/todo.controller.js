2f945a7a38afeff427f24d82b04fa2a82d9ab443
Hemanth A M <hemanth.a.m20@gmail.com>
2026-09-06 10:12:00 +0530

    Get, Update, Delete implemented

--- todo/backend/src/controller/todo.controller.js
+++ todo/backend/src/controller/todo.controller.js
@@ -1,5 +1,6 @@
 import pool from "../config/db.js";
 
+// Create
 const createTodo = async (req, res) => {
   try {
     const { description, completed } = req.body;
@@ -17,7 +18,72 @@ const createTodo = async (req, res) => {
   }
 };
 
-export { createTodo };
+// Get all
+const getTodos = async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM TODO");
+    res.status(200).json(allTodos.rows);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({
+      success: false,
+      message: "Server Error, please try again later",
+    });
+  }
+};
+
+//Update a Todo
+const updateTodo = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description, completed } = req.body;
+    const updatedTodo = await pool.query(
+      "update todo set description = $1, completed = $2 where todo_id = $3 returning *",
+      [description, completed, id],
+    );
+    if (updatedTodo.rows.length === 0) {
+      return res.status(404).json({
+        message: "Todo not found",
+      });
+    }
+    res.status(201).json({
+      message: "Todo updated successfully",
+      todo: updatedTodo.rows[0],
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Server Error, please try again later",
+    });
+  }
+};
+
+//Delete a todo
+const deleteTodo = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedTodo = await pool.query(
+      "DELETE FROM todo WHERE todo_id = $1 RETURNING *",
+      [id],
+    );
+    if (deletedTodo.rows.length === 0) {
+      return res.status(404).json({
+        message: "Todo not found",
+      });
+    }
+    res.status(200).json({
+      message: "Todo was deleted!",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Server Error, please try again later",
+    });
+  }
+};
+
+export { createTodo, getTodos, updateTodo, deleteTodo };
+
 //Notes:
 // req.body is the data that the client (frontend) sends to your backend in the request body.
 
@@ -26,3 +92,5 @@ export { createTodo };
 // RETURNING * : After inserting it, give me the entire newly created row.
 
 // So [0] doesn't mean the first task in the database. It means: Give me the first row returned by this particular query.
+
+// req.params is data that comes from the URL itself
--- todo/backend/src/routes/todo.routes.js
+++ todo/backend/src/routes/todo.routes.js
@@ -1,10 +1,27 @@
 import { Router } from "express";
 import pool from "../config/db.js";
-import { createTodo } from "../controller/todo.controller.js";
+import {
+  createTodo,
+  getTodos,
+  updateTodo,
+  deleteTodo,
+} from "../controller/todo.controller.js";
 
 // Notes: A router is basically a way to group related API routes.
 const router = Router();
 
+// Create
 router.post("/", createTodo);
 
+// Get all
+router.get("/", getTodos);
+
+//Update a todo
+router.put("/:id", updateTodo);
+
+//Delete a todo
+router.delete("/:id", deleteTodo);
+
 export default router;
+
+// Notes:
